9ad1d8deae9970c8ba823fb4f2d9ab801b2d81c3
tejasvichalke28 <tejasvichalke28@gmail.com>
2023-02-17 14:29:35 +0530

    one more program addded

---
+++ testcase/BreakContinueDemo.java
@@ -0,0 +1,22 @@
+package com.demo.testcase;
+
+public class BreakContinueDemo {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++)
+		{
+			if(i == 9)
+				break;
+			System.out.println(">> i = "+i);
+			
+			for(int j=1; j<=5; j++)
+			{
+				if(j==3)
+					continue;
+				System.out.println("j = "+j);
+			}	
+		}	
+	}
+	}
+
+
